e20adcf88922b86ddb950812b17d9e373a248d63
bogdan <bogdandemchenko@gmail.com>
2018-05-28 21:45:01 +0300

    thread have replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,5 +19,20 @@
                 </div>
             </div>
         </div>
+        <hr>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @foreach($thread->replies as $reply)
+                    <div class="card">
+                        <div class="card-header">
+                            {{ $reply->created_at->diffForHumans() }}
+                        </div>
+                        <div class="card-body">
+                            {{ $reply->body }}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </div>
 @endsection
