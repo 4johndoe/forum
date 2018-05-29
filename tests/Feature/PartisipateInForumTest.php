bd5aad52aa829193886bf725f3764745ffcf3e2e
bogdan demchenko <bogdandemchenko@gmail.com>
2018-05-29 18:27:13 +0300

    add partisipateInForumTest

---
+++ tests/Feature/PartisipateInForumTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PartisipateInForumTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function an_authenticated_user_may_participate_in_forum_threads()
+    {
+        $this->be($user = factory('App\User')->create());
+
+        $thread = factory('App\Thread')->create();
+
+        $reply = factory('App\Reply')->create();
+        $this->post('/threads/' . $thread->id . '/replies', $reply->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($reply->body);
+    }
+}
